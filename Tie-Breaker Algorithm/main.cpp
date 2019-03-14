9f236707cb2a4536fb8ad11f7f8e80650f50cf92
Dylaann <dyl.mrph@gmail.com>
2019-03-14 16:21:04 +0000

    n proccesses added to tie breaker

--- Tie-Breaker Algorithm/main.cpp
+++ Tie-Breaker Algorithm/main.cpp
@@ -6,11 +6,42 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
+#include <vector>
 
 using namespace std;
 
 bool flags[2] = { false, false };
 int last = 1;
+
+int const n = 3;
+int nFlags[n] { 0 };
+int nLast[n] { 0 };
+
+int cs(int i) {
+
+	while (true) {
+		for (int j = 0; j < n; j++) {
+
+			nFlags[i] = j;
+			nLast[j] = i;
+
+			for (int k = 0; k < n; k++) {
+				if (i != k) {
+					while (nFlags[k] >= nFlags[i] && nLast[j] == i) {
+						continue;
+					}
+				}
+			}
+		}
+
+		//Critical section
+		std::cout << i << std::endl;
+		nFlags[i] = -1;
+		//Non Critical section
+	}
+
+	return 1;
+}
 
 int cs1() {
 
@@ -53,11 +84,23 @@ int cs2() {
 
 int main() {
 
-	std::thread thread1(cs1);
-	std::thread thread2(cs2);
+	vector<std::thread> m_nThreads;
 
-	thread1.join();
-	thread2.join();
+	// Pertersons Algorithm
+	//std::thread thread1(cs1);
+	//std::thread thread2(cs2);
+
+	//thread1.join();
+	//thread2.join();
+
+	// N Proccesses Algorithm
+	for (int i = 0; i < n; i++) {
+		m_nThreads.push_back(std::thread(cs, i));
+	}
+
+	for (int i = 0; i < (int)m_nThreads.size(); i++) {
+		m_nThreads.at(i).join();
+	}
 
 	system("PAUSE");
 	return 0;
